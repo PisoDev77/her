8c819372d84c5a42ac59f0fec5c01248f4f69508
Pisodev77 <pisodev0427@gmail.com>
2024-01-09 16:39:20 +0900

    feat: Remove menuIcon / Link to Logo

--- src/components/Hangman/Header.jsx
+++ src/components/Hangman/Header.jsx
@@ -1,14 +1,14 @@
 import { IconButton, Stack } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
 	return (
 		<header id='hangman-header'>
 			<Stack direction='row' justifyContent='space-between'>
-				<h1>Hang Man</h1>
-				<IconButton aria-label='menu-info' size='large'>
-					<MenuIcon />
-				</IconButton>
+				<Link to={'/hang-man'}>
+					<h1>Hangmans</h1>
+				</Link>
 			</Stack>
 		</header>
 	);
