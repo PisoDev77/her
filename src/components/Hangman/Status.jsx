d20a1ae3e949c5765ff0b45d176ee13e315f0a4e
Pisodev77 <pisodev0427@gmail.com>
2024-01-09 16:37:19 +0900

    feat: Status Container for AnswerForm and GameBtn
    - Answerform & GameButton are in Status component

---
+++ src/components/Hangman/Status.jsx
@@ -0,0 +1,11 @@
+import AnswerForm from './AnswerForm';
+import GameButton from './GameButton';
+
+export default function Status() {
+	return (
+		<section>
+			<GameButton />
+			<AnswerForm />
+		</section>
+	);
+}
