3f272a699883d0a24c9d126e7bfc29803897c9b3
Pisodev77 <pisodev0427@gmail.com>
2024-01-09 16:38:52 +0900

    feat: Organize tree of project

--- src/components/Hangman/InfoMenu.jsx
+++
@@ -1,3 +0,0 @@
-export default function InfoMenu() {
-	return <section>This is Hang man Section InfoMenu.</section>;
-}
--- src/pages/Hangman.jsx
+++ src/pages/Hangman.jsx
@@ -1,22 +1,32 @@
-import Header from '../components/Hangman/Header';
-import View from '../components/Hangman/View';
-import InfoMenu from '../components/Hangman/InfoMenu';
+import { useState } from 'react';
+
 import { Grid } from '@mui/material';
 
+import HangmanContext from '../contexts/HangmanContext';
+
+import Header from '../components/Hangman/Header';
+import View from '../components/Hangman/View';
+import Status from '../components/Hangman/Status';
+
 export default function Hangman() {
+	const [answer, setAnswer] = useState('Test Word');
+	const [isGame, setIsGame] = useState(false);
+
 	return (
-		<main id='hang-man-app'>
-			<Grid container spacing={2}>
-				<Grid item xs={12} md={12}>
-					<Header />
+		<HangmanContext.Provider value={{ answer, setAnswer, isGame, setIsGame }}>
+			<main id='hang-man-app'>
+				<Grid container spacing={2}>
+					<Grid item xs={12} md={12}>
+						<Header />
+					</Grid>
+					<Grid item xs={10} md={3}>
+						<Status />
+					</Grid>
+					<Grid item xs={12} md={9}>
+						<View />
+					</Grid>
 				</Grid>
-				<Grid item xs={12} md={9}>
-					<View />
-				</Grid>
-				<Grid item xs={10} md={3}>
-					<InfoMenu />
-				</Grid>
-			</Grid>
-		</main>
+			</main>
+		</HangmanContext.Provider>
 	);
 }
