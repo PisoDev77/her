1390c7d17ed45ca55d5761db29f0098128596afa
Pisodev77 <pisodev0427@gmail.com>
2024-01-29 15:45:15 +0900

    conf: initialize project

--- src/pages/Hangman.jsx
+++ src/pages/Hangman.jsx
@@ -2,31 +2,18 @@ import { useState } from 'react';
 
 import { Grid } from '@mui/material';
 
-import HangmanContext from '../contexts/HangmanContext';
-
-import Header from '../components/Hangman/Header';
-import View from '../components/Hangman/View';
-import Status from '../components/Hangman/Status';
-
 export default function Hangman() {
 	const [answer, setAnswer] = useState('Test Word');
-	const [isGame, setIsGame] = useState(false);
 
 	return (
-		<HangmanContext.Provider value={{ answer, setAnswer, isGame, setIsGame }}>
-			<main id='hang-man-app'>
-				<Grid container spacing={2}>
-					<Grid item xs={12} md={12}>
-						<Header />
-					</Grid>
-					<Grid item xs={10} md={3}>
-						<Status />
-					</Grid>
-					<Grid item xs={12} md={9}>
-						<View />
-					</Grid>
-				</Grid>
-			</main>
-		</HangmanContext.Provider>
+		<main id='hang-man-app'>
+			<h1>Hang Man</h1>
+			<section>
+				<h2>Game Board</h2>
+			</section>
+			<section>
+				<h2>Game Status</h2>
+			</section>
+		</main>
 	);
 }
