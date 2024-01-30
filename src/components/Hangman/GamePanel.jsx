5f3c15a3f5b0e88e2f74e06713f992ab970b7d2f
Pisodev77 <pisodev0427@gmail.com>
2024-01-30 15:43:41 +0900

    feat & fix: GamePanel
    - previous code worked well but has side effect on letterboxes and
      answerword state
    - By using `useEffect`, solve the problem

--- src/components/Hangman/GamePanel.jsx
+++ src/components/Hangman/GamePanel.jsx
@@ -1,69 +1,74 @@
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { HangManContext } from '../../contexts';
-import { Card, Stack, TextField } from '@mui/material';
-
-const makeLetters = (answerWord) => answerWord.split('').map((alphabet) => ({ alphabet, guessed: false }));
+import { Card, TextField } from '@mui/material';
+import { Stack } from '@mui/system';
 
 export default function GamePanel() {
-	const { answerWord, cnt, setCnt } = useContext(HangManContext);
+	const { answerWord } = useContext(HangManContext);
+	const [idxs, setIdxs] = useState(answerWord.split('').map((alphabet) => (alphabet.trim() === '' ? -2 : -1)));
+	const [guessedAlphabet, setGuessedAlphabet] = useState('');
 
-	const letterBoxes = answerWord.split('').map((alphabet) => ({ alphabet, guessed: false }));
+	useEffect(() => {
+		setIdxs(answerWord.split('').map((alphabet) => (alphabet.trim() === '' ? -2 : -1)));
+	}, [answerWord]);
 
-	const guessAlphabet = (event) => {
+	const handleGuess = (event) => {
 		if (event.key !== 'Enter') return;
 
 		const guessedAlphabet = event.target.value.toLowerCase();
-		if (guessedAlphabet.trim() === '') return;
 
-		const answerAlphabetidxs = [];
-		let idx = answerWord.toLowerCase().indexOf(guessedAlphabet);
-		while (idx !== -1) {
-			answerAlphabetidxs.push(idx);
-			idx = answerWord.indexOf(guessedAlphabet, idx + 1);
+		if (guessedAlphabet.trim() === '') {
+			event.target.value = '';
+			return;
 		}
 
-		answerAlphabetidxs.forEach((idx) => {
-			letterBoxes[idx] = { ...letterBoxes[idx], guessed: true };
-		});
-		event.target.value = '';
+		let idx = answerWord.toLowerCase().indexOf(guessedAlphabet);
+		while (idx !== -1) {
+			idxs[idx] = idxs[idx] === -2 ? -2 : idx;
+			idx = answerWord.toLowerCase().indexOf(guessedAlphabet, idx + 1);
+		}
+		setIdxs([...idxs]);
 
-		if (answerAlphabetidxs.length === 0) setCnt(cnt - 1);
+		event.target.value = '';
+	};
+
+	const handleChange = (event) => {
+		if (!/[a-zA-Z]/.test(event.target.value)) {
+			setGuessedAlphabet('');
+		} else {
+			setGuessedAlphabet(event.target.value);
+		}
 	};
 
 	return (
 		<Stack spacing={{ xs: 0, sm: 1 }} direction='column' alignItems={'center'}>
-			<h2>Hang man game Panel</h2>
-			<div>기회: {cnt}</div>
-			{LetterBoxes(letterBoxes)}
-			{GuessAlphabet(guessAlphabet)}
+			<h1>Panel</h1>
+			<Stack spacing={{ xs: 0, sm: 1 }} direction='row' justifyContent={'center'}>
+				{idxs.map((idx, _) =>
+					//prettier-ignore
+					<Card key={_} sx={{ marginY: '1rem', paddingX: '1rem', paddingTop: '1rem', fontSize: '3rem' }}>
+						{idx === -2 ? (	<> &nbsp;&nbsp; </> ) 
+                                    : (	<> {idx === -1 
+                                            ? <> &nbsp;&nbsp; </> 
+                                            : answerWord[idx]}
+						                    <hr style={{ width: '100%' }} />
+                                        </>
+						)}
+					</Card>
+				)}
+			</Stack>
+			<TextField
+				variant='filled'
+				autoFocus={true}
+				autoComplete='off'
+				defaultValue={guessedAlphabet}
+				inputProps={{
+					maxLength: 1,
+					onKeyDown: handleGuess,
+					onChange: handleChange,
+					style: { textAlign: 'center', width: '1em' },
+				}}
+			/>
 		</Stack>
-	);
-}
-
-function LetterBoxes(letterBoxes) {
-	return (
-		<Stack spacing={{ xs: 0, sm: 1 }} direction='row' useFlexGap flexWrap='wrap'>
-			{letterBoxes.map(({ alphabet, guessed }, idx) => (
-				<Card
-					key={'LetterBox ' + idx}
-					variant='outlined'
-					sx={{ marginY: '1rem', paddingX: '1rem', paddingTop: '1rem' }}
-				>
-					{guessed ? alphabet : <>&nbsp;&nbsp;</>}
-					{alphabet.trim() !== '' && <hr style={{ width: '100%' }} />}
-				</Card>
-			))}
-		</Stack>
-	);
-}
-
-function GuessAlphabet(onKeyDown) {
-	return (
-		<TextField
-			variant='filled'
-			autoFocus={true}
-			autoComplete='off'
-			inputProps={{ onKeyDown, maxLength: 1, style: { textAlign: 'center', width: '1em' } }}
-		></TextField>
 	);
 }
