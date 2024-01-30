6af556ddd944977fdfeb85ab3b93a0c1681e5f5b
Pisodev77 <pisodev0427@gmail.com>
2024-01-30 15:43:09 +0900

    feat: Add Home Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Hangman from './pages/Hangman';
+import Home from './pages/Home';
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
+				<Route path='/' element={<Home />} />
 				<Route path='/hang-man' element={<Hangman />} />
 			</Routes>
 		</BrowserRouter>
