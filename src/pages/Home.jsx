e3d99d46aedd0f2daabf3f4491b9b2e29942b6fc
Pisodev77 <pisodev0427@gmail.com>
2024-01-31 15:49:34 +0900

    feat: Home
    - styling Home page component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,24 @@
+import { List, ListItemButton, ListItemAvatar, Avatar, ListItemText, Stack, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 export default function Home() {
 	return (
-		<main>
-			<ul>
-				<li>
-					<Link to={'/hangman'}>Hang Man</Link>
-				</li>
-			</ul>
-		</main>
+		<Stack spacing={{ xs: 0, sm: 1 }} direction='column' alignItems={'center'}>
+			<List>
+				<ListItemButton component={Link} to={'/hangman'}>
+					<ListItemAvatar>
+						<Avatar>H</Avatar>
+					</ListItemAvatar>
+					<ListItemText
+						primary={
+							<Typography variant='h3' component={'h2'}>
+								Hang Man
+							</Typography>
+						}
+						secondary='This is hang man game description.'
+					/>
+				</ListItemButton>
+			</List>
+		</Stack>
 	);
 }
