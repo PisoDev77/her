ed47794b7d43b62c19d6d37dccf4548001e8bc4c
Pisodev77 <pisodev0427@gmail.com>
2024-02-01 11:34:07 +0900

    feat: add Schedule Context API

--- src/contexts/index.js
+++ src/contexts/index.js
@@ -2,4 +2,6 @@ import { createContext } from 'react';
 
 export const HangManContext = createContext({});
 
+export const ScheduleContext = createContext({});
+
 export const ColorModeContext = createContext({ toggleColormode: () => {} });
