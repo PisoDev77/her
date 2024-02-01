f929800bacce95c7c09f86831c47c90be8266054
Pisodev77 <pisodev0427@gmail.com>
2024-02-01 13:05:33 +0900

    feat: Remove vite.svg Chage mobile

--- src/components/Schedule/SetLecture.jsx
+++ src/components/Schedule/SetLecture.jsx
@@ -1,30 +1,34 @@
 import { Stack, TextField } from '@mui/material';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { ScheduleContext } from '../../contexts';
 import { DefaultLecture, DeleteLecture } from './Lecture';
 import { getContrastColor, getRandomColor } from '../lib/random';
 
 export default function SetLecture() {
+	const [newLecture, setNewlecture] = useState('');
 	const { lectures, setLectures } = useContext(ScheduleContext);
 
 	const handleAddLecture = (e) => {
 		if (e.key !== 'Enter') return;
 
-		const lecture = e.target.value;
+		const inputLecture = e.target.value;
 
-		if (lecture.trim() === '' || lectures.find(({ lecture: _ }) => _ === lecture)) return;
+		if (inputLecture.trim() === '' || lectures.find(({ lecture }) => lecture === inputLecture)) return;
 
 		const backgroundColor = getRandomColor(lectures);
 		const color = getContrastColor(backgroundColor);
 
-		setLectures([...lectures, { lecture, color, backgroundColor }]);
+		setLectures([...lectures, { lecture: inputLecture, color, backgroundColor }]);
 		e.target.value = '';
 	};
 
 	return (
 		<Stack spacing={{ xs: 0, sm: 1 }} direction='column' alignItems={'flex-start'}>
 			<h1>Step 2. 교과 설정하기</h1>
-			<TextField onKeyDown={handleAddLecture} inputProps={{ style: { textAlign: 'center' } }} />
+			<TextField
+				defaultValue={newLecture}
+				inputProps={{ style: { textAlign: 'center' }, onKeyDown: handleAddLecture }}
+			/>
 			<Stack spacing={{ xs: 0, sm: 1 }} direction='row'>
 				{lectures &&
 					lectures.map(({ lecture, color, backgroundColor }, idx) =>
