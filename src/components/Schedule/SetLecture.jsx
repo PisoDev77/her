72a85b9e3beb5b40d56aaee93a1b00f62b248a5e
Pisodev77 <pisodev0427@gmail.com>
2024-02-01 13:35:21 +0900

    fix: form으로 변경

--- src/components/Schedule/SetLecture.jsx
+++ src/components/Schedule/SetLecture.jsx
@@ -1,46 +1,59 @@
-import { Stack, TextField } from '@mui/material';
+import { Button, FormControl, IconButton, Stack, TextField } from '@mui/material';
 import { useContext, useState } from 'react';
 import { ScheduleContext } from '../../contexts';
 import { DefaultLecture, DeleteLecture } from './Lecture';
 import { getContrastColor, getRandomColor } from '../lib/random';
+import { Add } from '@mui/icons-material';
 
 export default function SetLecture() {
-	const [newLecture, setNewlecture] = useState('');
+	const [newLecture, setNewLecture] = useState('');
 	const { lectures, setLectures } = useContext(ScheduleContext);
 
 	const handleAddLecture = (e) => {
-		alert(e.key);
-		if (e.key !== 'Enter') return;
-		const inputLecture = e.target.value;
+		e.preventDefault();
 
-		if (inputLecture.trim() === '' || lectures.find(({ lecture }) => lecture === inputLecture)) return;
+		if (newLecture.trim() === '' || lectures.find(({ lecture }) => lecture === newLecture)) return;
 
 		const backgroundColor = getRandomColor(lectures);
 		const color = getContrastColor(backgroundColor);
 
-		setLectures([...lectures, { lecture: inputLecture, color, backgroundColor }]);
-		e.target.value = '';
+		setLectures([...lectures, { lecture: newLecture, color, backgroundColor }]);
+		setNewLecture('');
 	};
 
 	return (
 		<Stack spacing={{ xs: 0, sm: 1 }} direction='column' alignItems={'flex-start'}>
 			<h1>Step 2. 교과 설정하기</h1>
-			<TextField
-				defaultValue={newLecture}
-				inputProps={{ style: { textAlign: 'center' }, onKeyDown: handleAddLecture }}
-			/>
+			<form onSubmit={handleAddLecture}>
+				<Stack spacing={{ xs: 0, sm: 1 }} direction='row'>
+					<TextField
+						autoComplete='off'
+						value={newLecture}
+						inputProps={{ style: { textAlign: 'center' }, onChange: (e) => setNewLecture(e.target.value) }}
+					/>
+					<IconButton type='submit'>
+						<Add />
+					</IconButton>
+				</Stack>
+			</form>
 			<Stack spacing={{ xs: 0, sm: 1 }} direction='row'>
 				{lectures &&
 					lectures.map(({ lecture, color, backgroundColor }, idx) =>
 						idx !== 0 ? (
 							<DeleteLecture
+								key={'lecture-' + idx}
 								lecture={lecture}
 								idx={idx}
 								color={color}
 								backgroundColor={backgroundColor}
 							/>
 						) : (
-							<DefaultLecture lecture={lecture} color={color} backgroundColor={backgroundColor} />
+							<DefaultLecture
+								key={'lecture-' + idx}
+								lecture={lecture}
+								color={color}
+								backgroundColor={backgroundColor}
+							/>
 						)
 					)}
 			</Stack>
