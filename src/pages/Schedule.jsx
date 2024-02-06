4f0a26064de0017471c82b19d62a42dbab9c8afc
Pisodev77 <pisodev0427@gmail.com>
2024-02-06 12:56:02 +0900

    feat: steps state

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -7,33 +7,37 @@ import TimeTable from '../components/Schedule/TimeTable';
 import { ScheduleContext } from '../contexts';
 
 export default function Schedule() {
+	const [term, setTerm] = useState({
+		startDate: null,
+		endDate: null,
+		dates: [],
+		days: 0,
+	});
+	const [step, setStep] = useState(0);
 	const [lectures, setLectures] = useState([{ lecture: '없음' }]);
 	const [lunch, setLunch] = useState(4);
-	const [timeTable, setTimeTable] = useState({
-		timeLectures: [
-			[0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0],
-		],
-	});
+	const [timeTable, setTimeTable] = useState([
+		[0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0],
+	]);
+
+	const stepEle = [<SetTerm />, <SetLecture />, <SetWeek />];
+
 	return (
-		<ScheduleContext.Provider value={{ lectures, lunch, setLunch, setLectures, timeTable, setTimeTable }}>
+		<ScheduleContext.Provider
+			value={{ term, setTerm, lectures, lunch, setLunch, setLectures, timeTable, setTimeTable, setStep }}
+		>
 			<Page
 				title={'Schedule'}
 				tabs={[
 					{
 						tabName: 'Setting',
-						tabElement: (
-							<>
-								<SetTerm />
-								<SetLecture />
-								<SetWeek />
-							</>
-						),
+						tabElement: stepEle[step],
 					},
 					{ tabName: 'Time Table', tabElement: <TimeTable /> },
 				]}
