f7e38c0767b707c0e40b1b67b70ac49b5c0a1ab9
Pisodev77 <pisodev0427@gmail.com>
2024-02-06 15:44:16 +0900

    datejs 관련 함수

--- src/lib/date.js
+++ src/lib/date.js
@@ -12,12 +12,17 @@ export const getFullDaysAndDays = (startDate, endDate) => {
 	const end = dayjs(endDate);
 
 	let days = 1;
+	let weeks = 1;
 	const fullDays = end.diff(start, 'day');
 
 	for (let idx = 0; idx < fullDays; idx++) {
 		const day = start.add(idx, 'day').get('day');
 		if (day > 0 && day < 6) days++;
+		if (day === 0) weeks++;
 	}
 
-	return [fullDays + 1, days];
+	return [fullDays + 1, fullDays + 1 < 0 ? 0 : days, weeks];
 };
+
+export const getDate = (date) =>
+	dayjs(date).isValid() ? date.get('month') + 1 + '월 ' + date.get('date') + '일' : null;
